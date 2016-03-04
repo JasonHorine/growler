109a53236017ae71ba63cd9c51670b310f53cb2b
Antonia Verdi <averdi@gm.slc.edu>
2016-03-04 12:11:03 -0600

    posts now add successfully from new to homepage

--- app/controllers/growls_controller.rb
+++ app/controllers/growls_controller.rb
@@ -2,6 +2,7 @@ class GrowlsController < ApplicationController
   set :views, Proc.new { File.join(root, "views/growls")}
 
 get '/growls' do
+  @growls = Growl.all
   erb :index
 end
 
@@ -9,8 +10,11 @@ get '/new' do
   erb :grr
 end
 
-
-
+post '/growls' do
+  growl = Growl.new(params["growl"])
+  growl.save
+  redirect '/growls'
+end
 
 
 
